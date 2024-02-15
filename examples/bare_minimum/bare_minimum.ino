bffa3ebdc363d5a0211436be440949c2a5fa8e96
Pio Baettig <baettigp@gmx.net>
2024-02-15 21:47:56 +0100

    Update bare_minimum.ino
    removed #ifdefs

--- examples/bare_minimum/bare_minimum.ino
+++ examples/bare_minimum/bare_minimum.ino
@@ -1,6 +1,4 @@
 #include <AcaiaArduinoBLE.h>
-#define FELICITA_ARC
-//define ACAIA
 
 AcaiaArduinoBLE acaia;
 void setup() {
@@ -9,16 +7,15 @@ void setup() {
   Serial.println("Scale Interface test");
   // Optionally add your Mac Address as an argument: acaia.init("##:##:##:##:##:##");
   acaia.init();
+  acaia.tare();
+  acaia.tare();
 }
 
 void loop() {
-
-  #ifdef ACAIA
   // Send a heartbeat message to the acaia periodically to maintain connection
   if (acaia.heartbeatRequired()) {
     acaia.heartbeat();
   }
-#endif
 
   // always call newWeightAvailable to actually receive the datapoint from the scale,
   // otherwise getWeight() will return stale data
